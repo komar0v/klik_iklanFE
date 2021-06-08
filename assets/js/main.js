366797d2d8c55f890c49d37d82fb16ed07999b43
komarov <fransiscodiaz81@gmail.com>
2021-06-09 01:06:36 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,18 @@
-(function($) {
+function minimize_sidebar(){
+	if (document.getElementById("sidebar")) {    
+		document.getElementById("sidebar").setAttribute("id", "sidebar_min");
 
-	"use strict";
+		document.getElementById("icn_text_home").innerHTML = '<span><img width="25" height="25" src="../assets/images/icons/home_icon.svg"></span>';
+		document.getElementById("icn_text_message").innerHTML = '<span><img width="25" height="25" src="../assets/images/icons/message_icon.svg"></span>';
+		document.getElementById("icn_text_trx").innerHTML = '<span><img width="25" height="25" src="../assets/images/icons/transaction_icon.svg"></span>';
+		document.getElementById("icn_text_support").innerHTML = '<span><img width="25" height="25" src="../assets/images/icons/support_icon.svg"></span>';
+	}
+	else {
+		document.getElementById("sidebar_min").setAttribute("id", "sidebar");
 
-	var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
-	$('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
-  });
-
-})(jQuery);
+		document.getElementById("icn_text_home").innerHTML = '<span><img width="25" height="25" src="../assets/images/icons/home_icon.svg"></span>Home';
+		document.getElementById("icn_text_message").innerHTML = '<span><img width="25" height="25" src="../assets/images/icons/message_icon.svg"></span>Message';
+		document.getElementById("icn_text_trx").innerHTML = '<span><img width="25" height="25" src="../assets/images/icons/transaction_icon.svg"></span>Transaction';
+		document.getElementById("icn_text_support").innerHTML = '<span><img width="25" height="25" src="../assets/images/icons/support_icon.svg"></span>Support';
+	}
+}
