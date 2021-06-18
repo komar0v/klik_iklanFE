ebd610a1b00e82bb4612c58e192a9d458b49eb31
komarov <fransiscodiaz81@gmail.com>
2021-06-19 01:25:02 +0700

    add profile talents

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -11,11 +11,16 @@ $(document).ready(function () {
       e.preventDefault();
     });
 
-    if (currentSectionIndex === 3) {
+    if (currentSectionIndex==1){
+        $("#head_payment").html("<img src='https://i.postimg.cc/NFYvG4vB/payment-peoples.png' class='img-fluid' width='400' heigt='100%'>")
+
+    }
+
+    if (currentSectionIndex === 2) {
       
       $(document).find(".form-wrapper .section").first().addClass("is-active");
       $(document).find(".steps li").first().addClass("is-active");
-      $("#modalPayment").modal('toggle');
+      $("#modalPayment").modal("toggle");
     }
   });
 });
